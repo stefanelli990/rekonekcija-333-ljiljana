be520539000e64d31789bfc866ab34bd3b1c583f
stefanelli990 <moxy990@gmail.com>
2023-12-08 02:52:02 +0100

    fixing api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,14 +12,14 @@
         <div class="relative">
           <button name="" id="" class="bg-gray-100 outline-none rounded-lg py-3 px-4 w-full flex justify-between items-center" @click="toggleFirstCurrency">
             <div class="flex items-center space-x-2">
-              <Icon icon="twemoji:flag-european-union" class="text-3xl"/><span>{{ firstCurrency }}</span>
+              <Icon icon="twemoji:flag-european-union" class="text-3xl"/><span>{{ initialCurrencyValueFirst }}</span>
             </div>
             <Icon icon="octicon:chevron-down-16" />
           </button>
           <ul class="absolute w-full mt-2 h-[221px] overflow-auto bg-gray-100 rounded-lg border border-gray-300" v-show="firstCurrencyIsShown">
             <li @click="selectFirstCurrency(currency, rate)" class=" py-3 px-4 w-full border-b border-gray-300 hover:bg-gray-400" v-for="(rate, currency) in currencyData.conversion_rates" :key="currency">
               <div class="flex items-center space-x-2">
-                <img  class="h-10 w-10" alt=""><span>{{ currency }}</span>
+                <img   class="h-10 w-10" alt=""><span>{{ currency }}</span>
               </div>
             </li>
           </ul>
@@ -31,14 +31,14 @@
         <div class="relative">
           <button name="" id="" class="bg-gray-100 outline-none rounded-lg py-3 px-4 w-full flex justify-between items-center" @click="toggleSecondCurrency">
             <div class="flex items-center space-x-2">
-              <Icon icon="twemoji:flag-european-union" class="text-3xl"/><span>{{ secondCurrency }}</span>
+              <Icon icon="twemoji:flag-european-union" class="text-3xl"/><span>{{ initialCurrencyValueSecond }}</span>
             </div>
             <Icon icon="octicon:chevron-down-16" />
           </button>
           <ul class="absolute w-full mt-2 h-[221px] overflow-auto bg-gray-100 rounded-lg border border-gray-300" v-show="secondCurrencyIsShown">
             <li @click="selectSecondCurrency(currency, rate)" class=" py-3 px-4 w-full border-b border-gray-300 hover:bg-gray-400" v-for="(rate, currency) in currencyData.conversion_rates" :key="currency">
               <div class="flex items-center space-x-2">
-                <img  class="h-10 w-10" alt=""><span>{{ currency }}</span>
+                <img :src="flagURL" class="h-10 w-10" alt=""><span>{{ currency }}</span>
               </div>
             </li>
           </ul>
@@ -57,25 +57,27 @@ import { onMounted, ref } from 'vue'
 import { Icon } from '@iconify/vue'
 
 const currencyData = ref([])
+
+const initialCurrencyValueFirst = ref('USD')
+const initialCurrencyValueSecond = ref('EUR')
+
 const firstCurrencyIsShown = ref(false)
 const secondCurrencyIsShown = ref(false)
-const firstCurrency = ref('USD')
-const secondCurrency = ref('EUR')
+
 const firstCurrencyRate = ref(null)
 const secondCurrencyRate = ref(null)
-const amount = ref(0)
+
+
+
+const amount = ref(1)
+
 const result = ref(0)
 
+const flagURL = ref(null)
+const currencyCode = ref('usd')
 
-const fetchData = () => {
-  fetch(`https://v6.exchangerate-api.com/v6/95779948034a4c1b35731e6c/latest/${firstCurrency.value}`)
-  .then(response => response.json())
-  .then(data => {
-    
-    currencyData.value = data
-    console.log(currencyData.value)
-  })
-}
+
+
 
 
 
@@ -87,23 +89,49 @@ const toggleSecondCurrency = () => {
 }
 
 const selectFirstCurrency = (currency,rate) => {
-  firstCurrency.value = currency
+  initialCurrencyValueFirst.value = currency
   firstCurrencyRate.value = rate
   console.log(rate)
 }
 
 const selectSecondCurrency = (currency,rate) => {
-  secondCurrency.value = currency
+  initialCurrencyValueSecond.value = currency
   secondCurrencyRate.value = rate
   console.log(rate)
 }
 
 const calculate = () => {
-  result.value = Number(((secondCurrencyRate / firstCurrencyRate) * amount.value).toFixed(2));
+  result.value = ((secondCurrencyRate.value / firstCurrencyRate.value) * amount.value).toFixed(2);
   console.log(result.value)
+}
+
+const fetchData = async () => {
+  fetch(`https://v6.exchangerate-api.com/v6/95779948034a4c1b35731e6c/latest/${initialCurrencyValueFirst.value}`)
+  .then(response => response.json())
+  .then(data => {
+    
+    currencyData.value = data
+    firstCurrencyRate.value = data.conversion_rates.USD
+    secondCurrencyRate.value = data.conversion_rates.EUR
+    console.log(initialCurrencyValueFirst.value)
+    console.log(currencyData.value)
+    console.log(firstCurrencyRate.value,secondCurrencyRate.value)
+  })
+}
+
+const fetchFlags = async () => {
+  fetch(`https://restcountries.com/v3.1/currency/${currencyCode.value}`)
+  .then(response => response.json())
+  .then(data => {
+ 
+
+    console.log(data[0].flags.svg)
+    flagURL.value = data[0].flags.svg
+  })
 }
 
 onMounted(() => {
   fetchData()
+  fetchFlags()
 })
 </script>
