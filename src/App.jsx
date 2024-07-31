d5fcd8fb69d5b95e0b61c516d5502f05455acef4
stefanelli990 <stefan.mohenski@gmail.com>
2024-07-31 22:28:27 +0200

    fixing seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import { HelmetProvider } from "react-helmet-async"
-import Seo from "./components/Seo"
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-import Reconnection from "./components/Reconnection"
-import Quote from "./components/Quote"
-import Treatmants from "./components/Treatmants"
-import Benefits from "./components/Benefits"
-import About from "./components/About"
-import Faq from "./components/Faq"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
+import { HelmetProvider } from "react-helmet-async";
+import Seo from "./components/Seo";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Reconnection from "./components/Reconnection";
+import Quote from "./components/Quote";
+import Treatmants from "./components/Treatmants";
+import Benefits from "./components/Benefits";
+import About from "./components/About";
+import Faq from "./components/Faq";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <HelmetProvider>
-      <Seo 
-      title="Rekonekcija 333 Ljiljana | Otkrijte Moć Rekonekcijskog Isceljenja"
-      description="Otkrijte transformativnu moć rekonekcijskog isceljivanja. Iskusite holističko zdravlje i duhovno poravnanje uz naše stručne praktičare. Istražite naše usluge, recenzije klijenata i rezervišite svoju seansu već danas za obnovljeni osećaj ravnoteže i harmonije."
-      keywords="Rekonekcijsko isceljivanje, lična rekonekcija, isceljenje, rekonekcija, holističko zdravlje, duhovno poravnanje, isceliteljske usluge"
-      image="/og-image.jpg"
-      url="https://rekonekcija333ljiljana.com"
-      />
+     
       <Navbar />
       <Hero />
       <Reconnection />
--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -3,7 +3,6 @@ import { Helmet } from 'react-helmet-async';
 export default function Seo({ title, description, keywords, image, url }) {
   return (
     <Helmet>
-    <title>{title}</title>
     <meta name="description" content={description} />
     <meta name="keywords" content={keywords} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -12,8 +11,10 @@ export default function Seo({ title, description, keywords, image, url }) {
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
     <meta property="og:image" content={image} />
+    <meta property="og:image:alt" content="A description of the image" />
     <meta property="og:url" content={url} />
     <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Rekonekcija 333 Ljiljana" />
   </Helmet>
   )
 }
