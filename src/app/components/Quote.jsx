1e8039b39f10bde7d6dc2adb5bb2081f381f8f4a
stefanelli990 <stefan.mohenski@gmail.com>
2024-08-05 16:10:54 +0200

    fixed quotation

--- src/app/components/Quote.jsx
+++ src/app/components/Quote.jsx
@@ -9,11 +9,11 @@ export default function Quote() {
         >
           <blockquote className="max-w-5xl text-white mx-auto p-4">
             <p className="italic">
-              “Uđite. Lezite i zatvorite oči. Pustite sebe da plutate, ali
+            &quot;Uđite. Lezite i zatvorite oči. Pustite sebe da plutate, ali
               nemojte zaspati. Verujte, onaj ko sluša misli i molitve sasvim je
               izvesno već čuo Vaše. Ne samo da je čuo sve ono za šta ste se
               molili, nego je čuo i ono za šta se niste ni setili da se molite.
-              On sve zna..."
+              On sve zna...&quot;
             </p>
             <footer className="mt-4">
               <p>- Erik Perl, osnivač Rekonekcije</p>
