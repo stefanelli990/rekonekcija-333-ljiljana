20c9764e93e66b8ff221d918454c22f71bd1e24f
stefanelli990 <stefan.mohenski@gmail.com>
2024-08-05 18:47:59 +0200

    added key to metadta

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
+// layouts.js
+
 import "./globals.css"
 import Head from 'next/head'
 
 export const metadata = {
   title: "Rekonekcija 333 Ljiljana | Otkrijte Moć Rekonekcijskog Isceljenja",
-  description: "Iskusite moć rekonekcijskog isceljivanja. Osetite holističko zdravlje i duhovno poravnanje uz naše stručne praktičare. Rezervišite seansu već danas.",
+  description: "Otkrijte transformativnu moć rekonekcijskog isceljivanja. Iskusite holističko zdravlje i duhovno poravnanje uz naše stručne praktičare. Istražite naše usluge, recenzije klijenata i rezervišite svoju seansu već danas za obnovljeni osećaj ravnoteže i harmonije.",
   icons: [
     {
       rel: 'icon',
@@ -29,18 +31,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-
+        <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content="rekonekcijsko isceljivanje, lična rekonekcija, isceljenje, rekonekcija, holističko zdravlje, duhovno poravnanje, isceliteljske usluge" />
         <meta name="author" content="Ljiljana Medović" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         {/* Open Graph tags for social media sharing */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="/og-image.jpg" />
-        <meta property="og:url" content="https://rekonekcija333ljiljana.com/" />
-        <meta property="og:type" content="website" />
+        <meta key="og:title" property="og:title" content={metadata.title} />
+        <meta key="og:description" property="og:description" content={metadata.description} />
+        <meta key="og:image" property="og:image" content="/og-image.jpg" />
+        <meta key="og:url" property="og:url" content="https://rekonekcija333ljiljana.com/" />
+        <meta key="og:type" property="og:type" content="website" />
 
       </Head>
       <body>{children}</body>
