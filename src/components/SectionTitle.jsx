43ec4c3ed3dd64e096abe090fe4b9e0c310f84ee
stefanelli990 <stefan.mohenski@gmail.com>
2024-10-31 14:27:38 +0100

    fixed margin on section title

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,6 +1,6 @@
 export default function SectionTitle({children}) {
   return (
-    <div className="max-w-2xl mx-auto mb-[50px] space-y-6 md:mb-[75px] md:text-center">
+    <div className="max-w-2xl mx-auto mb-[50px] space-y-6 md:text-center">
           {children}
         </div>
   )
